3e562a07f4ccbd7071cd53411d1bee8f1b2a6526
mhmh55516 <mhmh55516@gmail.com>
2020-05-07 15:08:31 +0200

    Update me.sh

--- me.sh
+++ me.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -s -e -p "Enter Target: "  target
-git clone https://github.com/j3ssie/Osmedeus
+//git clone https://github.com/j3ssie/Osmedeus
 cd Osmedeus
 ./install.sh
 ./osmedeus.py -t $target
