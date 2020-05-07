b95e2b93783c3aa14f156bcc4d6ec63fdde43f65
mhmh55516 <mhmh55516@gmail.com>
2020-05-07 18:06:42 +0200

    Delete me.sh

--- me.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -e -p "Enter Target: "  target
-git clone https://github.com/j3ssie/Osmedeus
-cd Osmedeus
-./install.sh
-wget -O/etc/chromium-browser/default https://raw.githubusercontent.com/mhmh55516/mexmos/master/chromium-flags
-./osmedeus.py -t $target
