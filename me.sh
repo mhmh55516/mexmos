7b89b3f602c384a6d79b407d27f34137e0c65fc5
mhmh55516 <mhmh55516@gmail.com>
2020-05-07 15:11:18 +0200

    Update me.sh

--- me.sh
+++ me.sh
@@ -2,7 +2,7 @@
 
 read -p "Enter the files you would like to install: " arg1
 read -s -e -p "Enter Target: "  target
-//git clone https://github.com/j3ssie/Osmedeus
-cd Osmedeus
-./install.sh
-./osmedeus.py -t $target
+#git clone https://github.com/j3ssie/Osmedeus
+#cd Osmedeus
+#./install.sh
+#./osmedeus.py -t $target
