52afd93932b426e3b7736fffa955eba095b3287f
mhmh55516 <mhmh55516@gmail.com>
2020-05-30 16:02:23 +0200

    Update general.py

--- general.py
+++ general.py
@@ -38,7 +38,7 @@ def scan_ip_nmap(option):
 		os.system("nmap '"+ipadd+"' -p '"+portadd+"' -Pn -sV -sC -A -oN '/root/.osmedeus/workspaces/"+option+"/portscan/nmap-"+option+".txt' --append-output --min-rate=1000 --max-retries=1")
 		
 def brute_subdomain(option):
-    os.system("gobuster dns -d '"+option+"' -t 5000 -c -w '/root/Desktop/uploads/wordlist/sub_list_9.6M/all.txt' -o '/root/.osmedeus/workspaces/"+option+"/subdomain/force-"+option+"-gobuster.txt' && sed -i 's/Found: //g' 'force-"+option+"-gobuster.txt' && cat 'final-"+option+".com.txt' 'force-"+option+"-gobuster.txt' | sort -u -o 'final-"+option+".txt'")
+    os.system(f"/root/go/bin/gobuster dns -d '"+option+"' -t 5000 -w '/root/Desktop/uploads/wordlist/sub_list_9.6M/all.txt' -o '/root/.osmedeus/workspaces/"+option+"/subdomain/force-"+option+"-gobuster.txt' && sed -i 's/Found: //g' 'force-"+option+"-gobuster.txt' && cat 'final-"+option+".com.txt' 'force-"+option+"-gobuster.txt' | sort -u -o 'final-"+option+".txt'")
 
 def single_handle(options):
     subdomain.SubdomainScanning(options)
