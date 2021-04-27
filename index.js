2bcc40a9f498d98a8a40b8f9dd677902eb6ba9a3
mhmh55516 <mhmh55516@gmail.com>
2021-04-27 23:42:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function reqListener () {
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "http://169.254.169.254.xip.io/latest/meta-data/ami-id"); 
+oReq.open("GET", "https://169.254.169.254.xip.io/latest/meta-data/ami-id"); 
 oReq.send();
