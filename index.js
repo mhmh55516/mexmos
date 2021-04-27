f63ea901d5fe0bdbae0a33de019fd394029440c0
mhmh55516 <mhmh55516@gmail.com>
2021-04-27 21:18:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function reqListener () {
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "http://localtest.me:8000/"); 
+oReq.open("GET", "http://localtest.me:8080/"); 
 oReq.send();
